7ebc95d2122d619e9de3fb6d2f7b0d4c4841a702
Mohith-Chilakala <1rn21cs044.mohith@gmail.com>
2024-01-12 05:45:29 +0530

    changed-min-width-in-phone

--- src/component/todo/todoList.jsx
+++ src/component/todo/todoList.jsx
@@ -3,7 +3,7 @@ import TodoCard from "./todo-card";
 const TodoList = ({todoList, deleteTodo}) => {
   return (
     <div className="flex">
-      <ul className="w-1/3 min-w-52 m-auto mt-10">
+      <ul className="w-1/3 min-w-80 m-auto mt-10">
         {todoList.map((todo) => {
           return (
             <li key={todo.id}>
